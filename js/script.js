5e239b782146f68861d59bf9afdb63b59182927f
PatrykBaranski <pbaranski740@gmail.com>
2023-08-30 11:39:24 +0200

    Print messages for player and computer

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let computerMove = "kamien";
+let playerMove = "papier";
+printMessage(
+  `Zagrałem ${computerMove}! Jeśli Twój ruch to papier, to wygrywasz!`
+);
+printMessage(`Ruch gracza to ${playerMove}`);
