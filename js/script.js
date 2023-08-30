f60ee492cc3cc3ffc36344e57ab5f7e0c1b4de6d
PatrykBaranski <pbaranski740@gmail.com>
2023-08-30 17:41:13 +0200

    Add logic for computer move in game

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-const computerMove = "kamien";
-const playerMove = "papier";
-printMessage(
-  `Zagrałem ${computerMove}! Jeśli Twój ruch to papier, to wygrywasz!`
-);
-printMessage(`Ruch gracza to ${playerMove}`);
+const randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log("Wylosowana liczba to: " + randomNumber);
+let computerMove = "nieznany ruch";
+if (randomNumber === 1) {
+  computerMove = "kamień";
+} else if (randomNumber === 2) {
+  computerMove = "papier";
+} else if (randomNumber === 3) {
+  computerMove = "nozyce";
+}
+printMessage(`Mój ruch to: ${computerMove}`);
