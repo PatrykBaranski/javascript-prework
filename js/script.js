784ba0d0b9c8da3954f432e0c0712609696a3472
PatrykBaranski <pbaranski740@gmail.com>
2023-08-30 18:05:21 +0200

    Add logic for choosing a winner in game

--- js/script.js
+++ js/script.js
@@ -8,3 +8,28 @@ const playerInput = prompt(
 const playerMove = changeNumberToMove(+playerInput);
 console.log(`Gracz wybral liczbe: ${playerInput}`);
 printMessage(`Twoj ruch to: ${playerMove}`);
+const computerWin = "Ja wygrałem";
+const playerWin = "Ty wygrłeś";
+const draw = "remis";
+let winner = "nie mozna wybrac zwyciezcy";
+
+if (playerMove === "kamień" && computerMove === "papier") {
+  winner = computerWin;
+} else if (playerMove === "papier" && computerMove === "papier") {
+  winner = draw;
+} else if (playerMove === "nozyce" && computerMove === "papier") {
+  winner = playerWin;
+} else if (playerMove === "kamień" && computerMove === "kamień") {
+  winner = draw;
+} else if (playerMove === "papier" && computerMove === "kamień") {
+  winner = playerWin;
+} else if (playerMove === "nozyce" && computerMove === "kamień") {
+  winner = computerWin;
+} else if (playerMove === "kamień" && computerMove === "nozyce") {
+  winner = playerWin;
+} else if (playerMove === "papier" && computerMove === "nozyce") {
+  winner = computerWin;
+} else if (playerMove === "nozyce" && computerMove === "nozyce") {
+  winner = draw;
+}
+printMessage(winner);
