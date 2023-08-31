6a22f29039301c33af1c7506a6863d800c8cf0cc
PatrykBaranski <pbaranski740@gmail.com>
2023-08-31 15:32:00 +0200

    Code cleaning

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 const randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log("Wylosowana liczba to: " + randomNumber);
-const computerMove = changeNumberToMove(randomNumber);
-printMessage(`Mój ruch to: ${computerMove}`);
 const playerInput = prompt(
   "Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce."
 );
+const computerMove = changeNumberToMove(randomNumber);
 const playerMove = changeNumberToMove(+playerInput);
-console.log(`Gracz wybral liczbe: ${playerInput}`);
+printMessage(`Mój ruch to: ${computerMove}`);
 printMessage(`Twoj ruch to: ${playerMove}`);
 displayResults(computerMove, playerMove);
