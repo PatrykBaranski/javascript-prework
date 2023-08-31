9be969e92b83c4c060567b3769d36d7ae4fa33a7
PatrykBaranski <pbaranski740@gmail.com>
2023-08-31 19:20:02 +0200

    Add buttons for choosing a option in game

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-const randomNumber = Math.floor(Math.random() * 3 + 1);
-const playerInput = prompt(
-  "Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce."
-);
-const computerMove = changeNumberToMove(randomNumber);
-const playerMove = changeNumberToMove(+playerInput);
-printMessage(`Mój ruch to: ${computerMove}`);
-printMessage(`Twoj ruch to: ${playerMove}`);
-displayResults(computerMove, playerMove);
+const rockBtn = document.getElementById("play-rock");
+const paperBtn = document.getElementById("play-paper");
+const scissorsBtn = document.getElementById("play-scissors");
+const playGame = (playerInput) => {
+  clearMessages();
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
+  const computerMove = changeNumberToMove(randomNumber);
+  const playerMove = changeNumberToMove(+playerInput);
+  printMessage(`Mój ruch to: ${computerMove}`);
+  printMessage(`Twoj ruch to: ${playerMove}`);
+  displayResults(computerMove, playerMove);
+};
+rockBtn.addEventListener("click", () => {
+  playGame(1);
+});
+paperBtn.addEventListener("click", () => {
+  playGame(2);
+});
+scissorsBtn.addEventListener("click", () => {
+  playGame(3);
+});
