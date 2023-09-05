2624a5f1e1e76b1468197debdaa53a6767a2e850
PatrykBaranski <pbaranski740@gmail.com>
2023-09-05 13:58:59 +0200

    Code cleaing

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,16 @@
-function printMessage(msg) {
+const printMessage = (msg) => {
   const div = document.createElement("div");
   div.innerHTML = msg;
   document.getElementById("messages").appendChild(div);
-}
+};
 
-function clearMessages() {
+const clearMessages = () => {
   document.getElementById("messages").innerHTML = "";
-}
+};
 const changeNumberToMove = (number) => {
   let move = "nieznany ruch";
   if (number === 1) {
-    move = "kamień";
+    move = "kamien";
   } else if (number === 2) {
     move = "papier";
   } else if (number === 3) {
@@ -22,25 +22,11 @@ const displayResults = (computerMove, playerMove) => {
   const computerWin = "Ja wygrałem";
   const playerWin = "Ty wygrłeś";
   const draw = "remis";
-  let winner = "nie mozna wybrac zwyciezcy";
-  if (playerMove === "kamień" && computerMove === "papier") {
-    winner = computerWin;
-  } else if (playerMove === "papier" && computerMove === "papier") {
-    winner = draw;
-  } else if (playerMove === "nozyce" && computerMove === "papier") {
-    winner = playerWin;
-  } else if (playerMove === "kamień" && computerMove === "kamień") {
-    winner = draw;
-  } else if (playerMove === "papier" && computerMove === "kamień") {
-    winner = playerWin;
-  } else if (playerMove === "nozyce" && computerMove === "kamień") {
-    winner = computerWin;
-  } else if (playerMove === "kamień" && computerMove === "nozyce") {
-    winner = playerWin;
-  } else if (playerMove === "papier" && computerMove === "nozyce") {
-    winner = computerWin;
-  } else if (playerMove === "nozyce" && computerMove === "nozyce") {
-    winner = draw;
-  }
+  const output = {
+    kamien: { kamien: draw, nozyce: playerWin, papier: computerWin },
+    papier: { papier: draw, kamien: computerWin, nozyce: playerWin },
+    nozyce: { nozyce: draw, kamien: playerWin, papier: computerWin },
+  };
+  const winner = output[computerMove][playerMove];
   printMessage(winner);
 };
--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
-const rockBtn = document.getElementById("play-rock");
-const paperBtn = document.getElementById("play-paper");
-const scissorsBtn = document.getElementById("play-scissors");
-const playGame = (playerInput) => {
-  clearMessages();
-  const randomNumber = Math.floor(Math.random() * 3 + 1);
-  const computerMove = changeNumberToMove(randomNumber);
-  const playerMove = changeNumberToMove(+playerInput);
-  printMessage(`Mój ruch to: ${computerMove}`);
-  printMessage(`Twoj ruch to: ${playerMove}`);
-  displayResults(computerMove, playerMove);
-};
-rockBtn.addEventListener("click", () => {
-  playGame(1);
-});
-paperBtn.addEventListener("click", () => {
-  playGame(2);
-});
-scissorsBtn.addEventListener("click", () => {
-  playGame(3);
-});
+{
+  const rockBtn = document.getElementById("play-rock");
+  const paperBtn = document.getElementById("play-paper");
+  const scissorsBtn = document.getElementById("play-scissors");
+
+  const playGame = (playerInput) => {
+    clearMessages();
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
+    const computerMove = changeNumberToMove(randomNumber);
+    const playerMove = changeNumberToMove(+playerInput);
+    printMessage(`Mój ruch to: ${computerMove}`);
+    printMessage(`Twoj ruch to: ${playerMove}`);
+    displayResults(computerMove, playerMove);
+  };
+  rockBtn.addEventListener("click", () => {
+    playGame(1);
+  });
+  paperBtn.addEventListener("click", () => {
+    playGame(2);
+  });
+  scissorsBtn.addEventListener("click", () => {
+    playGame(3);
+  });
+}
